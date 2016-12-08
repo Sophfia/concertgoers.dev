2f00d91977e66c4585fb99cc7a7764c2e553c92e
Sophfia <anguianosofia@msn.com>
2016-12-08 11:33:14 -0800

    Correction of previous master
    corrected the location of the custom post fields (echo
    types_render_field)
    -placed under div entry-content

--- wp-content/themes/acmephoto-child/template-parts/content-venue.php
+++ wp-content/themes/acmephoto-child/template-parts/content-venue.php
@@ -31,24 +31,6 @@ if( !has_post_thumbnail() ){
 							<?php if ( 'post' === get_post_type() ) : ?>
 								<div class="entry-meta">
 									<?php acmephoto_posted_on( 1, 1 ); ?>
-									<!-- the follwoing echo types_render_field(s) print the customfields that were created for the venue post-->
-									<div id="venue-content-area">
-										<div class="venue_contact_info">
-											<!--Displays the street, city, state zipcode of the venue breakline phone number-->
-											<?php echo types_render_field("venue-street", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
-											<?php echo types_render_field("venue-csz", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
-											<br>
-											<?php echo types_render_field("venue-pnum", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
-										</div>
-										<div class="venue_images">
-										<!--.Displays images of the venue-->
-											<?php echo types_render_field("venue-image", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?> 
-										</div>
-										<div class="venue_description">
-										<!--.Display description of the venue-->
-											<?php echo types_render_field("venue-description", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
-										</div>
-									</div><!--.entry venue area-->
 								</div><!-- .entry-meta -->
 							<?php endif; ?>
 						</div>
@@ -82,6 +64,24 @@ if( !has_post_thumbnail() ){
 	?>
 	<div class="entry-content">
 		<?php the_content(); ?>
+		<!-- the follwoing echo types_render_field(s) print the customfields that were created for the venue post-->
+									<div id="venue-content-area">
+										<div class="venue_contact_info">
+											<!--Displays the street, city, state zipcode of the venue breakline phone number-->
+											<?php echo types_render_field("venue-street", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
+											<?php echo types_render_field("venue-csz", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
+											<br>
+											<?php echo types_render_field("venue-pnum", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
+										</div>
+										<div class="venue_images">
+										<!--.Displays images of the venue-->
+											<?php echo types_render_field("venue-image", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?> 
+										</div>
+										<div class="venue_description">
+										<!--.Display description of the venue-->
+											<?php echo types_render_field("venue-description", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));?>
+										</div>
+									</div><!--.entry venue area-->
 		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'acmephoto' ),
